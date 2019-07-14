b631b767d9845430a506e3a34cf91e68b091933d
Aga Matyjasik <agus.wroclaw@gmail.com>
2019-07-14 23:09:19 +0200

    Add files for task-17.7

---
+++ config.js
@@ -0,0 +1,6 @@
+    
+module.exports = {
+	'GOOGLE_CLIENT_ID': '1004509306796-v9j7l6qaoss2d3assrpalc6dbi4v4a8f.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'ql7Ok-ze9TanOTOww-6kz1f2',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
